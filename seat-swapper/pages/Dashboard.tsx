61967d177973ee66d9a393a298583816ed9bee0c
techmin <46662857+techmin@users.noreply.github.com>
2021-11-13 20:48:19 -0600

    started on dashboard

---
+++ seat-swapper/pages/Dashboard.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { typography } from '@mui/system';
+const Dashboard = () => {
+    return (
+        <Typography  component="div"
+        >
+            <Typography variant="h1">
+                <Box sx={{textAlign:'center',m:1}}> SeatSwapper </Box>
+            </Typography> 
+            <Box
+                sx={{
+                    textAlign:'center',
+                    m:1,
+                    width: 400,
+                    height: 100,
+                    backgroundColor: 'primary.dark',
+                }}
+            >
+                Swap offer to A2(Window)
+
+            </Box>
+        </Typography>
+
+    )
+}
+
+export default Dashboard
